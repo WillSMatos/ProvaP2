9e3589627cccd84bd32412a56382dd05fabcc27a
Willian Saud <williansaud.bw7@gmail.com>
2025-06-09 16:10:14 -0300

    Foram adicionadas as classes (Produtos e Clientes)

---
+++ ProvaP2/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LojaVirtual.Models
+{
+    public class Cliente
+    {
+        public int Id { get; }
+        public string Nome { get; }
+        public string Email { get; }
+        public string CPF { get; }
+
+        public Cliente(int id, string nome, string email, string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+                throw new ArgumentException("Nome do cliente não pode ser vazio.");
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email do cliente não pode ser vazio.");
+            if (string.IsNullOrWhiteSpace(cpf))
+                throw new ArgumentException("CPF do cliente não pode ser vazio.");
+
+            Id = id;
+            Nome = nome;
+            Email = email;
+            CPF = cpf;
+        }
+    }
+}
---
+++ ProvaP2/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LojaVirtual.Models
+{
+    public class Produto
+    {
+        public int Id { get; }
+        public string Nome { get; }
+        public decimal Preco { get; }
+        public string Categoria { get; }
+
+        public Produto(int id, string nome, decimal preco, string categoria)
+        {
+            if (preco < 0)
+                throw new ArgumentException("Preço do produto não pode ser negativo.");
+            if (string.IsNullOrWhiteSpace(nome))
+                throw new ArgumentException("Nome do produto não pode ser vazio.");
+            if (string.IsNullOrWhiteSpace(categoria))
+                throw new ArgumentException("Categoria do produto não pode ser vazia.");
+
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Categoria = categoria;
+        }
+    }
+}
